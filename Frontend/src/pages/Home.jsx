1f8d3e760ed3072ae9539ae40fc99b355b67434e
9DFF-2B31 <136964344+9DFF-2B31@users.noreply.github.com>
2025-06-25 13:58:15 +0530

    banners

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from 'react';
+import MainBanner from '../components/MainBanner';
+import BestSeller from '../components/BestSeller';
 
 const Home = () => {
   return (
-    <div>
-      this is home
+    <div className='mt-10'>
+      <MainBanner />
+      <p>this is home</p>
+      <BestSeller />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
