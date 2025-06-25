797e7d03f7b36c4fb36a6534c763a7ae67553bbf
HarshAgrawal7672 <harshagrawal7895@gmail.com>
2025-06-25 16:06:13 +0530

    newsletter

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,21 +1,18 @@
-import React from 'react';
-import MainBanner from '../components/MainBanner';
-import BestSeller from '../components/BestSeller';
-import BottomBanner from '../components/BottomBanner';
-import Categories from '../components/Categories';
+import React from "react";
+import MainBanner from "../components/MainBanner";
+import BestSeller from "../components/BestSeller";
+import BottomBanner from "../components/BottomBanner";
+import Categories from "../components/Categories";
+import NewsLetter from "../components/NewsLetter";
 
 const Home = () => {
   return (
-    <div className='mt-10'>
+    <div className="mt-10">
       <MainBanner />
       <Categories />
-      <p>this is home</p>
       <BestSeller />
-
-
-
-
-      <BottomBanner/>
+      <BottomBanner />
+      <NewsLetter/>
     </div>
   );
 };
