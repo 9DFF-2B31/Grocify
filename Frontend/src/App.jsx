33d600b7cf888e9fd632278a025a88395a7f0c37
HarshAgrawal7672 <harshagrawal7895@gmail.com>
2025-06-25 13:14:30 +0530

    best seller section

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import {Toaster} from "react-hot-toast"
 
 const App = () => {
   return (
     <div>
       <Navbar/>
+
+      <Toaster />
       <div>
         <Routes>
           <Route path='/' element={<Home/>} />
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import BestSeller from '../components/BestSeller'
 
 const Home = () => {
   return (
     <div>
       this is home
+      <BestSeller/>
     </div>
   )
 }
