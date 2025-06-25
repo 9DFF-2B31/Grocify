29014c86f1c88aefc155b54a1e5862e3d12528d1
9DFF-2B31 <136964344+9DFF-2B31@users.noreply.github.com>
2025-06-25 14:52:18 +0530

    categories

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import MainBanner from '../components/MainBanner';
 import BestSeller from '../components/BestSeller';
 import BottomBanner from '../components/BottomBanner';
+import Categories from '../components/Categories';
 
 const Home = () => {
   return (
     <div className='mt-10'>
       <MainBanner />
+      <Categories />
       <p>this is home</p>
       <BestSeller />
 
