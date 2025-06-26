8e05fe87f5d37fd1152c80b8cbbdfcc0f30b8333
kishanbehera90 <beherakishann90@gmail.com>
2025-06-26 23:42:22 +0530

    Cart_icon_functionality

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -13,6 +13,7 @@ const Navbar = () => {
     setShowUserLogin,
     setSearchQuery,
     searchQuery,
+    getCartCount,
   } = useAppContext();
   const logout = async () => {
     setUser(null);
@@ -61,7 +62,7 @@ const Navbar = () => {
             className="w-6 opaacity-80"
           />
           <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">
-            3
+            {getCartCount()}
           </button>
         </div>
 
@@ -93,14 +94,29 @@ const Navbar = () => {
         )}
       </div>
 
-      <button
-        onClick={() => (open ? setOpen(false) : setOpen(true))}
-        aria-label="Menu"
-        className="sm:hidden"
-      >
-        {/* Menu Icon SVG */}
-        <img src={assets.menu_icon} alt="menu" />
-      </button>
+      <div className="flex items-center gap-6 sm:hidden">
+        <div
+          onClick={() => navigate("/cart")}
+          className="relative cursor-pointer"
+        >
+          <img
+            src={assets.nav_cart_icon}
+            alt="cart"
+            className="w-6 opaacity-80"
+          />
+          <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">
+            {getCartCount()}
+          </button>
+        </div>
+          <button
+            onClick={() => (open ? setOpen(false) : setOpen(true))}
+            aria-label="Menu"
+            className=""
+          >
+            {/* Menu Icon SVG */}
+            <img src={assets.menu_icon} alt="menu" />
+          </button>
+        </div>
 
       {/* Mobile Menu */}
       {open && (
