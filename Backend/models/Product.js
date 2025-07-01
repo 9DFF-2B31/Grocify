52741757389f61184b0f289bf84ff51cfa46132e
HarshAgrawal7672 <harshagrawal7895@gmail.com>
2025-07-01 12:52:55 +0530

    product api basic setup

---
+++ Backend/models/Product.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose"
+
+
+const productSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:Array,
+        required:true
+    },
+    price:{
+        type:Number,
+        required:true
+    },
+    offerprice:{
+        type:Number,
+        required:true
+    },
+    image:{
+        type:Array,
+        required:true
+        },
+    category:{
+        type:String,
+        required:true
+    },
+    instock:{
+        type:Boolean,
+        default:true
+    }
+},{
+    timestamps:true
+})
+
+const Product=mongoose.model.product || mongoose.model('product',productSchema)
+
+export default Product
--- Backend/server.js
+++ Backend/server.js
@@ -3,11 +3,13 @@ import express from 'express';
 import cors from 'cors'
 import connectDB from './configs/db.js';
 import 'dotenv/config'
+import connectCloudinary from './configs/cloudinary.js';
 const app=express();
 
 
 const port=process.env.PORT || 4000;
 await connectDB()
+await connectCloudinary()
 //allow multiple origins
 const allowedOrigins=['http://localhost:5173']
 //middleware configuration
